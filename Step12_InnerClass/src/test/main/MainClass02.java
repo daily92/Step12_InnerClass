d5d013157cc675be0be4a3268150abf8df5d409b
jiehee <daily92@hanmail.net>
2017-11-23 10:10:11 +0900

    MainClass02 수정

--- Step12_InnerClass/src/test/main/MainClass02.java
+++ Step12_InnerClass/src/test/main/MainClass02.java
@@ -1,5 +1,8 @@
 package test.main;
-
+/*
+ *  클래스 안에 클래스 inner class
+ *  메소드 안에 클래스 local inner class
+ */
 public class MainClass02 {
 	public static void main(String[] args) {
 		// 메소드 내부에서도 클래스를 정의할 수 있다.
